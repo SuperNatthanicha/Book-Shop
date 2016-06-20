11530a05e0a6401627b55a705f7abd9afe99209f
wahn <Natthanichawahn@gmail.com>
2016-06-20 16:47:18 +0700

    finish 20 June

--- app/src/main/java/neu/natthanicha/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/natthanicha/bookshop/SignUpActivity.java
@@ -2,12 +2,52 @@ package neu.natthanicha.bookshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit ประกาศตัวแปร
+    private EditText nameEditText, usernameEditText, passwordEditText;
+    private String nameString, usernameString,passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/neu/add_user_wahn.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        usernameEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        usernameString = usernameEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") ||usernameString.equals("") || passwordString.equals("")){
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง","กรุณากรอกช่องว่าง คะ");
+
+        } else {
+            //no Space
+            uploadToServer();
+
+        }
+
+    }   // clickSign
+
+    private void uploadToServer() {
+
     }
-}
+
+
+}   // main Class
